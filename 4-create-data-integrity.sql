52a299a7e54dec3b9dd8359afc3050d709ff4380
Artem-Garanin <artem.garanin@johnabbottcollege.net>
2020-10-30 13:55:02 -0400

    Create data integrity

---
+++ 4-create-data-integrity.sql
@@ -0,0 +1,34 @@
+-- switch to the Aragon2020 database
+USE Aragon2020
+;
+GO
+
+            -- Artem Garanin part
+
+/***** Table No. 1 - Employee.tblClass ****/
+
+/***** Table No. 2 - Employee.tblJobTitle ****/
+
+/***** Table No. 3 - Employee.tblEmployee ****/
+
+/***** Table No. 4 - Employee.tblEmployeeTraining ****/
+
+            -- Vladimir Tocari part
+
+/***** Table No. 5 - Customer.tblHealthPlan ****/
+
+/***** Table No. 6 - Customer.tblHousehold ****/
+
+/***** Table No. 7 - Customer.tblCustomer ****/
+
+            -- Ihor Stashchuk part
+
+/***** Table No. 8 - Prescription.tblClinic ****/
+
+/***** Table No. 9 - Prescription.tblDoctor ****/
+
+/***** Table No. 10 - Prescription.tblDrug ****/
+
+/***** Table No. 11 - Prescription.tblRx ****/
+
+/***** Table No. 12 - Prescription.tblRefill ****/
